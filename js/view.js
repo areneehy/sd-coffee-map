db1db5bfccd75a470c987745c79b670d83bd0dc8
Renee Hyde <areneehy@gmail.com>
2017-03-27 09:14:49 -0700

    update knockout bindings

--- js/view.js
+++ js/view.js
@@ -3,12 +3,16 @@ var locations = [];
 var infoWindows = [];
 var images = [];
 var markers = [];
-var hasBeer = [];
-var hasFood = [];
-var hasOutdoor = [];
-var hasParking= [];
+// var hasBeer = [];
+// var hasFood = [];
+// var hasOutdoor = [];
+// var hasParking= [];
 
-function viewModel() {
+function ViewModel() {
+
+    var self = this;
+    this.locationsArray = ko.observableArray([]);
+
     var sanDiego = {
         lat: 32.930639,
         lng: -117.4703734
@@ -18,8 +22,11 @@ function viewModel() {
         center: sanDiego
     });
     places.forEach(function(place) {
-        locations.push(new Location(place));
+        var newLocation = new Location(place);
+        locations.push(newLocation);
+        self.locationsArray.push(newLocation);
     });
+    // this.pix = ko.observable('');
     currentInfoWindow = infoWindows[0];
     for (var i = 0; i < markers.length; ++i) {
         google.maps.event.addListener(markers[i], 'click', function(innerKey) {
@@ -31,47 +38,78 @@ function viewModel() {
                     markers[innerKey].setAnimation(null);
                 }, 1450);
                 currentInfoWindow = infoWindows[innerKey];
-                $("#photos").html(images[innerKey]);
+                $("#photos").html(markers[innerKey].images);
+                // self.pix(markers[innerKey].images);
             };
         }(i));
     }
-    for (var i = 0; i < listView.length; ++i) {
-        listView[i].addEventListener('click', function(innerKey) {
-            return function() {
-                google.maps.event.trigger(markers[innerKey], 'click');
-            };
-        }(i));
+    self.clickMarker = function() {
+                google.maps.event.trigger(this.marker, 'click');
     }
-    var k;
-    for (var i = 0; i < locations.length; ++i) {
-        if (locations[i].hasBeer === true) {
-            k = document.getElementsByClassName("listItems");
-            console.log(k[i]);
-            k[i].className += " hasBeer";
-            hasBeer.push(locations[i].marker);
+    // var k;
+    // for (var i = 0; i < locations.length; ++i) {
+    //     if (locations[i].hasBeer === true) {
+    //         // k = document.getElementsByClassName("listItems");
+    //         // k[i].className += " hasBeer";
+    //         hasBeer.push(locations[i].marker);
+    //     }
+    //     if (locations[i].hasFood === true) {
+    //         // k = document.getElementsByClassName("listItems");
+    //         // k[i].className += " hasFood";
+    //         hasFood.push(locations[i].marker);
+    //     }
+    //     if (locations[i].hasOutdoor === true) {
+    //         // k = document.getElementsByClassName("listItems");
+    //         // k[i].className += " hasOutdoor";
+    //         hasOutdoor.push(locations[i].marker);
+    //     }
+    //     if (locations[i].hasParking === true) {
+    //         // k = document.getElementsByClassName("listItems");
+    //         // k[i].className += " hasParking";
+    //         hasParking.push(locations[i].marker);
+    //     }
+    // }
+
+    this.filterTermArray = ko.observableArray(['All Results','Has Food','Has Beer','Has Outdoor','Has Parking']);
+
+    this.filter = function(filterTerm) {
+        drop();
+        console.log(filterTerm);
+        var filterProperty;
+        if (filterTerm === 'Has Food') {
+            filterProperty = 'hasFood';
         }
-        if (locations[i].hasFood === true) {
-            k = document.getElementsByClassName("listItems");
-            console.log(k[i]);
-            k[i].className += " hasFood";
-            hasFood.push(locations[i].marker);
+        if (filterTerm === 'Has Beer') {
+            filterProperty = 'hasBeer';
         }
-        if (locations[i].hasOutdoor === true) {
-            k = document.getElementsByClassName("listItems");
-            console.log(k[i]);
-            k[i].className += " hasOutdoor";
-            hasOutdoor.push(locations[i].marker);
+        if (filterTerm === 'Has Outdoor') {
+            filterProperty = 'hasOutdoor';
         }
-        if (locations[i].hasParking === true) {
-            k = document.getElementsByClassName("listItems");
-            console.log(k[i]);
-            k[i].className += " hasParking";
-            hasParking.push(locations[i].marker);
+        if (filterTerm === 'Has Parking') {
+            filterProperty = 'hasParking';
         }
-    }
+        if (filterTerm === 'All Results') {
+            filterProperty = 'showAll';
+        }
+
+        self.locationsArray().forEach(function(location) {
+            if (location[filterProperty] === true) {
+                location.showLocation(true);
+                location.marker.setVisible(true);
+            } else {
+                location.showLocation(false);
+                location.marker.setVisible(false);
+            }
+            // console.log(location.marker);
+        });
+
+    };
+
 }
 
+
 var Location = function(place) {
+    var self = this;
     this.lat = place.lat;
     this.lng = place.lng;
     this.name = place.name;
@@ -79,6 +117,7 @@ var Location = function(place) {
     this.search = place.search;
     this.hasFood = place.hasFood;
     this.hasBeer = place.hasBeer;
+    this.showAll = true;
     this.hasParking = place.hasParking;
     this.hasOutdoor = place.hasOutdoor;
     this.latlng = new google.maps.LatLng(this.lat, this.lng);
@@ -87,10 +126,9 @@ var Location = function(place) {
         map: map
     });
     markers.push(this.marker);
-    console.log("location set");
-    name = '<div id="listView" class="listItems">%data%</div>';
-    this.formattedName = name.replace("%data%", this.name);
-    $("#list").append(this.formattedName);
+    this.showLocation = ko.observable(true);
+
+    this.images = "";
     var flickr = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=583683c6baa1b32fb35b05b46b82ef0c&text=' + this.search + '&per_page=12&format=json&jsoncallback=?';
     var imgs = "";
     $.ajax({
@@ -102,6 +140,8 @@ var Location = function(place) {
               var url = 'https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg';
               imgs += '<img src="' + url + '"/>';
           });
+          self.images = imgs;
+          self.marker.images = imgs;
           images.push(imgs);
         },
         error: function() {
@@ -121,55 +161,10 @@ function drop() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Sets map on markers in the selected array.
-function setMapOnAll(array, map) {
-    for (var i = 0; i < array.length; i++) {
-        array[i].setMap(map);
-    }
-}
-
-// Removes all markers and list items from view.
-function clear() {
-    drop();
-    setMapOnAll(markers, null);
-    $(".listItems").hide();
-}
-
-// show all markers and list items
-function resetAll() {
-    drop();
-    setMapOnAll(markers, map);
-    $(".listItems").show();
-}
-
-function showHasFood() {
-    clear();
-    setMapOnAll(hasFood, map);
-    $(".hasFood").show();
-}
-
-function showHasBeer() {
-  clear();
-  setMapOnAll(hasBeer, map);
-  $(".hasBeer").show();
-}
-
-function showHasOutdoor() {
-    clear();
-    setMapOnAll(hasOutdoor, map);
-    $(".hasOutdoor").show();
-}
-
-function showHasParking() {
-    clear();
-    setMapOnAll(hasParking, map);
-    $(".hasParking").show();
-}
-
 function mapError() {
-  alert("Google Maps has failed.");
+  alert('Google Maps has failed.');
 }
 
 var startMap = function() {
-    ko.applyBindings(new viewModel());
+    ko.applyBindings(new ViewModel());
 };
